65176ef00b163a91091878b9dc921dfa6fda0cc3
aparicioborjap-cpu <aparicioborjap@gmail.com>
2026-05-26 13:29:11 +0200

    Add browserLocalPersistence to Firebase auth

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,12 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, browserLocalPersistence, setPersistence } from 'firebase/auth';
 import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence);
 export const googleProvider = new GoogleAuthProvider();
 
 async function testConnection() {
@@ -17,5 +18,4 @@ async function testConnection() {
     }
   }
 }
-
 testConnection();
